869a6a3859b2901433cf4775b889f0ee461f236b
Chaitali Khangar <chaitali.khangar@gmail.com>
2016-09-28 09:00:29 +0530

    asset file added

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,7 +9,8 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-Rails.application.config.assets.precompile += %w( event.js )
+
 Rails.application.config.assets.precompile += %w( jquery.min.js )
 Rails.application.config.assets.precompile += %w( bootstrap-datepicker.js )
 Rails.application.config.assets.precompile += %w( datepicker.css )
+Rails.application.config.assets.precompile += %w( event.js )
